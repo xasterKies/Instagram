ac404f110c921f135ef08f036cad4b024c162788
TiokengSamuel <samueledward40@gmail.com>
2021-03-11 20:04:15 +0100

    firebase onfig

--- instagram/src/lib/firebase.js
+++ instagram/src/lib/firebase.js
@@ -4,7 +4,14 @@ import 'firebase/auth';
 
 // Here is going to be the seed file
 
-const config = {};
+const config = {
+    apiKey: "AIzaSyBtYzel_YGXvknavvbo5gudzcEEzxdGEas",
+    authDomain: "instagram-web-2d786.firebaseapp.com",
+    projectId: "instagram-web-2d786",
+    storageBucket: "instagram-web-2d786.appspot.com",
+    messagingSenderId: "721483244915",
+    appId: "1:721483244915:web:8adb3ea997cb8c1eb6d106"
+  };
 
 const firebase = Firebase.initializeApp(config);
 const { FieldValue } = Firebase.firestore;
