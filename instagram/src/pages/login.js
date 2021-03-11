3c7bce057158698f00a31ae6aa9d996a01f28997
TiokengSamuel <samueledward40@gmail.com>
2021-03-11 21:02:21 +0100

    setting upt states for values

--- instagram/src/pages/login.js
+++ instagram/src/pages/login.js
@@ -1,6 +1,18 @@
 import React from 'react'
+import { useState, useContext } from 'react'
+import { useHistory } from 'react-router-dom'
+import FirebaseContext from '../context/firebase'
 
 function Login() {
+    const history = useHistory();
+    const { firebase } = useContext(FirebaseContext);
+
+    const [emailAddress, setEmailAddress] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [error, setError] = useState('');
+    const isInvalid = password === '' || emailAddress === '';
+
     return (
         <div>
             <p>I am the Login</p>
