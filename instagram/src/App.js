3d099b7c0481b5c2613f056ab2e2fbcf10817a87
TiokengSamuel <samueledward40@gmail.com>
2021-03-11 20:32:41 +0100

    constant routes

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+const Login = lazy(() => import ('./pages/login'));
 
 function App() {
   return (
     <Router>
-      
+      <Suspense fallback={<p>Loading ... </p>}>
+        <Switch>
+          <Route path="/login" component={Login} />
+        </Switch>
+      </Suspense>
     </Router>
   );
 }
---
+++ instagram/src/constants/routes.js
@@ -0,0 +1,5 @@
+export const DASHBOARD = '/'
+export const SIGN_UP = '/signup'
+export const NOT_FOUND = '/not-found'
+export const PROFILE = '/p/:username'
+export const LOGIN = './login'
